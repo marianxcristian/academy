7174b8e9f1e519423082daa7c2508c89e1fa2c13
Marian Cristian <marianxcristian@gmail.com>
2025-01-06 10:07:40 +0000

    arrays methods

---
+++ javascript-fundamentals/arrays-methods/problems/18-two-d-product.js
@@ -0,0 +1,44 @@
+/*
+Write a function `twoDimensionalProduct(array)` that takes in a 2D array of
+numbers as an argument. The function should return the total product of all
+numbers multiplied together.
+
+Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
+methods.
+
+Examples:
+
+let arr1 = [
+  [6, 4],
+  [5],
+  [3, 1]
+];
+console.log(twoDimensionalProduct(arr1)); // 360
+
+let arr2 = [
+  [11, 4],
+  [2]
+];
+console.log(twoDimensionalProduct(arr2)); // 88
+*/
+
+let twoDimensionalProduct = function(arr) {
+    let product = arr.reduce(function(total, subArray){
+
+      let subTotal = subArray.reduce(function(subTotal, num){
+        return subTotal * num;
+      }, 1);
+
+      return total * subTotal;
+    }, 1);
+
+    return product;
+};
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+    module.exports = twoDimensionalProduct;
+} catch (e) {
+    module.exports = null;
+}
